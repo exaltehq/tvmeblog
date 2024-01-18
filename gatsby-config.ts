03abd8f9f2b19d98bcac0b81eccd75bb72484af9
exaltehq <exaltehq@gmail.com>
2024-01-17 19:12:45 -0500

    Info
    Updates

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,11 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Geanny's Blog`,
+    siteTitleAlt: `Geanny's Blog`,
+    siteHeadline: `Geanny's Blog`,
+    siteUrl: `https://tavarez.me`,
+    siteDescription: `Welcome to my blog, where I occasionally post about what's on my mind. Topics include business building, health, wealth building, and random cool pictures taken from my smartphone.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
@@ -56,7 +56,7 @@ const config: GatsbyConfig = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `desc`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
